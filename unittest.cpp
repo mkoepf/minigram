96556d54d82effe7a6d6eda24dfeaba73638e2f8
Michael Köpf <michael.koepf@cognotekt.com>
2015-12-10 10:15:34 +0100

    Initial

---
+++ unittest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Michael Köpf on 10.12.15.
+//
+
+#define BOOST_TEST_MODULE Currency
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_SUITE(UnitTestSuite)
+
+
+BOOST_AUTO_TEST_CASE(TestConstructorCurrencyAmountCurrency) {
+        BOOST_REQUIRE_EQUAL(2, (2*1));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
